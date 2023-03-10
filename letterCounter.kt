201ab685b6ac5b8d3a31fde650c1f1f84850f1d1
Ruben Hernandez <123031291+Ruben935@users.noreply.github.com>
2023-03-09 23:53:23 -0600

    Update letterCounter.kt

--- letterCounter.kt
+++ letterCounter.kt
@@ -1,16 +1,10 @@
 fun main(args: Array<String>)
 {
     print("Enter a string: \n")
-    val inputString = readLine() ?: ""
+    val inputString = readLine()!!
 
     print("Enter a character to search and count: \n")
-    val letter = readLine()?.getOrNull(0)
-
-    if (letter == null)
-    {
-        println("Error. Please enter a single character.")
-        return
-    }
+    val letter = readLine()!![0]
 
     val count = inputString.count { it == letter }
 
