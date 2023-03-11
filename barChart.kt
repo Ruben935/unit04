1d85e1dbd2413ae6aab8153cde944494ed591d8e
Ruben Hernandez <123031291+Ruben935@users.noreply.github.com>
2023-03-11 15:14:50 -0600

    Create barChart.kt

---
+++ barChart.kt
@@ -0,0 +1,21 @@
+fun main(args: Array<String>)
+{
+    val sales = IntArray(5)
+
+    for (i in 1..5)
+    {
+        print("Enter today's sales for store $i: ")
+        sales[i - 1] = readLine()!!.toInt()
+    }
+
+    println("SALES BAR CHART")
+    for (i in 1..5)
+    {
+        print("Store $i: ")
+        repeat(sales[i - 1] / 100)
+        {
+            print("*")
+        }
+        println()
+    }
+}
